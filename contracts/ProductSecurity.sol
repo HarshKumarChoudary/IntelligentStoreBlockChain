c7c74937f0828226613b5fc85dbf690226089234
Harsh kumar choudhary <harshkumarchoudhary144@gmail.com>
2023-03-25 15:14:28 +0530

    Added logic for bulk data at a time

--- contracts/ProductSecurity.sol
+++ contracts/ProductSecurity.sol
@@ -13,17 +13,24 @@ contract ProductSecurity {
     
     mapping(address => mapping(uint256 => Product)) public prodcuts;
 
-    function createProduct(address _owner, string memory _name, string memory _description, uint256 _isbn) public {
+    function createProduct(address _owner, string[] memory _name, string[] memory _description, uint256[] memory _isbn) public {
 
-        Product storage prodcut = prodcuts[_owner][_isbn];
+        uint256 len = _name.length;
 
-        require(prodcut.owner == address(0), "The ISBN number should not be same");
+        for(uint256 i = 0; i < len; ++ i){
+            Product storage prodcut = prodcuts[_owner][_isbn[i]];
+            require(prodcut.owner == address(0), "The ISBN Number shall be unique");
+        }
 
-        prodcut.owner = _owner;
-        prodcut.name = _name;
-        prodcut.description = _description;
-        prodcut.dateCreated = block.timestamp;
-        prodcut.extras = "";
+        for(uint256 i = 0; i < len; ++ i){
+            Product storage prodcut = prodcuts[_owner][_isbn[i]];
+            prodcut.owner = _owner;
+            prodcut.name = _name[i];
+            prodcut.description = _description[i];
+            prodcut.dateCreated = block.timestamp;
+            prodcut.extras = "";
+        }
+        
     }
 
     function getProduct(address _owner, uint256 isbn) public view returns (Product memory) {
